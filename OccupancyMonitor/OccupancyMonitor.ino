7ffcf1f6802bff273122cf573840c32a4945a7e0
Lemar Haddad <haddadlemar@gmail.com>
2022-10-13 19:17:31 +0800

    documented code

--- OccupancyMonitor/OccupancyMonitor.ino
+++ OccupancyMonitor/OccupancyMonitor.ino
@@ -12,38 +12,38 @@
 #define AWS_IOT_SUBSCRIBE_TOPIC "occupancymonitor/room1/sub"
 
 // Infrared sensors
-#define IR_CHAIR 33
-#define DIST_THRESH 75
-// LEDs
+#define IR_CHAIR 33 // pin for the infrared sensor on the chair
+#define DIST_THRESH 75 // infrared threshold for when the seat is occupied
+// Pins for LEDs
 #define RED_LED 13
 #define GREEN_LED 14
 
-// button
+// Pin for pushbutton
 #define BUTTON 23
-
-// timeouts
-uint32_t MOVEMENT_TIMEOUT = 1000;
 
 WiFiClientSecure wifi_client = WiFiClientSecure();
 MQTTClient mqtt_client = MQTTClient(256); //256 indicates the maximum size for packets being published and received.
 
-uint32_t t1;
+uint32_t t1; // records the timestamp of the last time a MQTT message was sent
 
-// counting
+// records occupancy - 0 if unoccupied, 1 if occupied
 int count;
 
 
-// button
-uint32_t button_ts;
-int button_press;
-int val;
-int buttonState;
+// button variables
+uint32_t button_ts; // the last timestamp the button was pushed
+int button_press; // whether the button is pushed or not
+int buttonState; // the state of the button
 
-// Ultrasonic sensor
-long duration; // variable for the duration of sound wave travel
-int distance; // variable for the distance measurement
+// IR sensor variables
+int val; // the measured value of the IR sensor
 
 
+/**
+ * @brief Connects to WiFi. Then connects to AWS IoT Core 
+ * to begin transmission of MQTT messages over the MQTT client.
+ * AWS public/private keys and WiFi SSID and password all set in config.h
+ */
 void connectAWS()
 {
   //Begin WiFi in station mode
@@ -90,6 +90,11 @@ void connectAWS()
   Serial.println("AWS IoT Connected!");
 }
 
+/**
+ * @brief Publishes a message over MQTT to the AWS_IOT_PUBLISH_TOPIC
+ * topic. The message contains the room_name and count.
+ * 
+ */
 void publishMessage()
 {
   //Create a JSON document of size 200 bytes, and populate it
@@ -113,22 +118,27 @@ void incomingMessageHandler(String &topic, String &payload) {
 
 void setup() {
   Serial.begin(115200); //115200
+  // set up pins
   pinMode(RED_LED, OUTPUT);
   pinMode(GREEN_LED, OUTPUT);
   digitalWrite(RED_LED, HIGH);
   digitalWrite(GREEN_LED, HIGH);
   pinMode(BUTTON, INPUT);
+  // make the button HIGH when unpressed, LOW when pressed (to ground)
   digitalWrite(BUTTON, HIGH);
+  // set the last time that the MQTT message was sent to now
   t1 = millis();
+  // set the room to unoccupied
   count = 0;
+  // connect to MQTT client
   connectAWS();
-//  digitalWrite(RED_LED, LOW);
   button_press = 0;
 }
 
 void loop() {
   mqtt_client.loop();
   
+  // turn on or off LEDs based on occupancy
   if(count > 0) {
     digitalWrite(GREEN_LED, HIGH);
     digitalWrite(RED_LED, LOW);
@@ -137,14 +147,17 @@ void loop() {
     digitalWrite(GREEN_LED, LOW);
   }
 
+  // check button state
   buttonState = digitalRead(BUTTON);
   //Serial.println(buttonState);
   if(buttonState == 0) {
     Serial.println("Button is pushed");
     button_press = 1;
+    // set the last time that the button was pushed
     button_ts = millis();
   }
 
+  // keep the seat as occupied for 30 seconds after the button was last pushed
   if(button_press == 1 && millis() > (button_ts + 30000)) {
     // button press lasts 30 seconds before being reset
     button_press = 0;
@@ -154,6 +167,8 @@ void loop() {
   val = analogRead(IR_CHAIR); //> threshold_entry;
   //Serial.println(val);
 
+  // seat is occupied if the button is pushed or the IR sensor reading is above
+  // the threshold
   if(button_press == 1 || val >= DIST_THRESH) {
     // condition met for occupancy of seat
     count = 1;
@@ -164,11 +179,9 @@ void loop() {
   delay(200);
   
 
-  // publish msg over MQTT
+  // publish msg over MQTT every 60 seconds
   if(millis() > t1){
     t1 = millis() + 60000;
     publishMessage();
   }
-
-
 }
